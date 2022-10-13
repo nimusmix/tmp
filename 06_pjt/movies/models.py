d544d9db7aaf6ad0e457059730441644f9343ec9
nimusmix <nimusmix@gmail.com>
2022-10-13 17:53:35 +0900

    pjt

---
+++ 06_pjt/movies/models.py
@@ -0,0 +1,14 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Movie(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+
+class Commment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
